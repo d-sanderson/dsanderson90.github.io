453ed4600f87058595be05a285fd706a7eec8ed5
dsanderson90 <dsanderson3@cnm.edu>
2019-10-09 11:15:33 -0600

    project headlines now change color when you hover over a project img

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // let dataset;
 // fetch('https://api.github.com/users/dsanderson90')
 //     .then(response => response.json())
@@ -18,91 +17,101 @@
 
 // }
 
-let greeting = document.querySelector(".greeting")
-let greetingStr = document.querySelector(".greeting").innerText;
+let greeting = document.querySelector('.greeting');
+let greetingStr = document.querySelector('.greeting').innerText;
 let greetingArr = greetingStr.split('');
 // let colors = ['#FF355E', '#FD5B78', '#0066FF', '#FF6037', '#FF9933', '#FFCC33', '#FFFF66', '#CCFF00', '#66FF66', '#AAF0D1', '#50BFE6', '#FF6EFF', '#EE34D2']
-let colors = ['orange', 'yellow', 'lime']
-let output0 = greetingArr.map( (el, i) => {
-  el = `<span class="ltr" style='color: ${getRandomColor()}'>${el}</span>`
+let colors = ['orange', 'yellow', 'lime'];
+let output0 = greetingArr.map((el, i) => {
+  el = `<span class="ltr" style='color: ${getRandomColor()}'>${el}</span>`;
   return el;
-})
+});
 greeting.innerHTML = output0.join('');
 
-let greetingShort = document.querySelector(".greeting.short");
-let greetingShortStr = document.querySelector(".greeting.short").innerText;
-let greetingShortArr = greetingShortStr.split('')
-let output = greetingShortArr.map( (el, i) => {
-  el = `<span class="ltr"style='color: ${getRandomColor()}'>${el}</span>`
+let greetingShort = document.querySelector('.greeting.short');
+let greetingShortStr = document.querySelector('.greeting.short').innerText;
+let greetingShortArr = greetingShortStr.split('');
+let output = greetingShortArr.map((el, i) => {
+  el = `<span class="ltr"style='color: ${getRandomColor()}'>${el}</span>`;
   return el;
-})
+});
 greetingShort.innerHTML = output.join('');
 
-let projectsTaglineShort = document.querySelector(".projects-tagline.short")
+let projectsTaglineShort = document.querySelector('.projects-tagline.short');
 let projectsTaglineShortStr = projectsTaglineShort.innerText;
 let projectsTaglineShortArr = projectsTaglineShortStr.split('');
-let output2 = projectsTaglineShortArr.map ( (el, i) => {
-  if(i % 2 == 0) {
-  el = `<span class="up" style='color: ${getRandomColor()}'>${el}</span>`
-  }
-  else {
-    el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`
+let output2 = projectsTaglineShortArr.map((el, i) => {
+  if (i % 2 == 0) {
+    el = `<span class="up" style='color: ${getRandomColor()}'>${el}</span>`;
+  } else {
+    el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`;
   }
   return el;
-
-})
-projectsTaglineShort.innerHTML = output2.join('')
+});
+projectsTaglineShort.innerHTML = output2.join('');
 
 let projectTagline = document.querySelector('.projects-tagline');
-let projectTaglineStr = document.querySelector('.projects-tagline').innerText
-let projectTaglineArr = projectTaglineStr.split('')
-let output4 = projectTaglineArr.map( (el, i) => {
-  if(i % 2 == 0) {
-    el = `<span class="up" style='color: ${getRandomColor()}'>${el}</span>`
-  }
-  else {
-    el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`
+let projectTaglineStr = document.querySelector('.projects-tagline').innerText;
+let projectTaglineArr = projectTaglineStr.split('');
+let output4 = projectTaglineArr.map((el, i) => {
+  if (i % 2 == 0) {
+    el = `<span class="up" style='color: ${getRandomColor()}'>${el}</span>`;
+  } else {
+    el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`;
   }
   return el;
-})
-projectTagline.innerHTML = output4.join('')
+});
+projectTagline.innerHTML = output4.join('');
 
 const toggleDarkLight = () => {
-  const body = document.getElementById("body");
+  const body = document.getElementById('body');
   let currentClass = body.className;
-  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
-}
+  body.className = currentClass == 'dark-mode' ? 'light-mode' : 'dark-mode';
+};
 document.getElementById('toggle').addEventListener('click', () => {
   toggleDarkLight();
   document.getElementById('lol').style.color = getRandomColor();
-
 });
 
-let contactTitleShort = document.querySelector(".contact-title.short");
-let contactTitleShortStr =contactTitleShort.innerText;
+let contactTitleShort = document.querySelector('.contact-title.short');
+let contactTitleShortStr = contactTitleShort.innerText;
 let contactShortArr = contactTitleShortStr.split('');
-let output3 = contactShortArr.map ( (el, i) => {
-  el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`
+let output3 = contactShortArr.map((el, i) => {
+  el = `<span class="down" style='color: ${getRandomColor()}'>${el}</span>`;
   return el;
-
-})
-contactTitleShort.innerHTML = output3.join('')
+});
+contactTitleShort.innerHTML = output3.join('');
 
 function getRandomColor() {
-
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
 // SOCIAL MEDIA ICONS EVENT LISTENERS
 
-let links = document.querySelectorAll('.link, .project-title, .up, .down').forEach(element => {
-  element.addEventListener('mouseover', () => {
-    element.style.color = getRandomColor();
+let links = document
+  .querySelectorAll('.link, .project-title, .up, .down')
+  .forEach(element => {
+    element.addEventListener('mouseover', () => {
+      element.style.color = getRandomColor();
+    });
+    element.addEventListener('mouseout', () => {
+      element.style.color = '';
+    });
   });
-  element.addEventListener('mouseout', () => {
-    element.style.color = '';
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach(img => {
+   img.addEventListener('mouseover', e => {
+    //selects project headline from the DOM
+    let projectText = e.target.parentNode.previousElementSibling.childNodes[1];
+    projectText.style.color = getRandomColor();
+  });
+  img.addEventListener('mouseout', e => {
+    let projectText = e.target.parentNode.previousElementSibling.childNodes[1];
+    projectText.style.color = '';
   });
 });
+
 
 // CONTACT SECTION ANIMATION
 
@@ -113,46 +122,48 @@ const consoleText = (words, id, colors) => {
   let letterCount = 1;
   let x = 1;
   let waiting = false;
-  let target = document.getElementById(id)
-  target.setAttribute('style', 'color:' + colors[0])
+  let target = document.getElementById(id);
+  target.setAttribute('style', 'color:' + colors[0]);
   window.setInterval(function() {
-
     if (letterCount === 0 && waiting === false) {
       waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       window.setTimeout(function() {
         let usedColor = colors.shift();
         colors.push(usedColor);
         let usedWord = words.shift();
         words.push(usedWord);
         x = 1;
-        target.setAttribute('style', 'color:' + colors[0])
+        target.setAttribute('style', 'color:' + colors[0]);
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 1000);
     } else if (letterCount === words[0].length + 1 && waiting === false) {
       waiting = true;
       window.setTimeout(function() {
         x = -1;
         letterCount += x;
         waiting = false;
-      }, 1000)
+      }, 1000);
     } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount)
+      target.innerHTML = words[0].substring(0, letterCount);
       letterCount += x;
     }
-  }, 120)
+  }, 120);
   window.setInterval(function() {
     if (visible === true) {
-      con.className = 'console-underscore hidden'
+      con.className = 'console-underscore hidden';
       visible = false;
-
     } else {
-      con.className = 'console-underscore'
+      con.className = 'console-underscore';
 
       visible = true;
     }
-  }, 400)
-}
-consoleText(['Contact Me.', 'Get In Touch.', 'Let\'s build something.', 'Hire me!'], 'text',[getRandomColor(), getRandomColor(), getRandomColor()]);
+  }, 400);
+};
+consoleText(
+  ['Contact Me.', 'Get In Touch.', "Let's build something.", 'Hire me!'],
+  'text',
+  [getRandomColor(), getRandomColor(), getRandomColor()]
+);
 
